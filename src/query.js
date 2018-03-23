02ca15b7c4131b3e750a60b7e1ced1258b41f799
evavro <me@madhax.io>
2018-03-23 02:57:00 -0700

    :sparkles: slightly improved clarity

--- src/query.js
+++ src/query.js
@@ -2,5 +2,5 @@ import { every } from './list'
 
 export const query = element => ({
   all: ()   => every(element),
-  by: (k,v) => every(element).filter(el => el[k] === v)
+  by: (k,v) => every(element).filter(elem => elem[k] === v)
 })
