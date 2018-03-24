8da76c55bd693f97eeba0e3158d9e9b025f6db52
evavro <me@madhax.io>
2018-03-23 20:39:00 -0700

    :wrench: :sparkles: es6 friendly index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-module.exports = {
-  notes  : require('./notes'),
-  scales : require('./scales'),
-  chords : require('./chords')
-}
+import notes from './notes'
+import scales from './scales'
+import chords from './chords'
+
+export { notes, scales, chords }
+export default { notes, scales, chords }
