0f33705f845035ba48a1f863be0fd10ce4d53ce4
evavro <me@madhax.io>
2018-03-23 21:02:30 -0700

    :microscope: :sparkles: :boom: removed now redundant mutation in list-spec

--- test/list-spec.js
+++ test/list-spec.js
@@ -3,14 +3,10 @@ const sinon = require('sinon')
 const test = require('tape')
 
 test('should pre-populate the memoization cache if invoked as a function', t => {
-  const orig = list.every
-
   list.every = sinon.spy()
 
   list()
 
   t.true(list.every.called)
   t.end()
-
-  list.every = orig
 })
