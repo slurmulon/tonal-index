85d2716c7665a30a51f66ea414173b2c12160e63
evavro <me@madhax.io>
2018-03-23 23:16:30 -0700

    :microscope: :cd: updated test fixtures

--- test/note-spec.js
+++ test/note-spec.js
@@ -1,26 +1,11 @@
 const { all } = require('../src/notes')
+const fixture = require('./fixtures/notes')
 const equal = require('deep-equal')
 const test = require('tape')
 
 test('all', t => {
   const result = all()
-  const expected = ['C',
-      'C#',
-      'Db',
-      'D',
-      'D#',
-      'Eb',
-      'E',
-      'F',
-      'F#',
-      'Gb',
-      'G',
-      'G#',
-      'Ab',
-      'A',
-      'A#',
-      'Bb',
-      'B']
+  const expected = fixture
 
   t.true(equal(result, expected))
   t.end()
