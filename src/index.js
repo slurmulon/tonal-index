b9921f6b6e092ebee277d7197f2e95a05969cb2c
evavro <me@madhax.io>
2018-03-24 01:14:26 -0700

    :package: :bug: fixed index module warm/lazy modes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-module.exports = {
+const package = {
   notes  : require('./notes'),
   scales : require('./scales'),
   chords : require('./chords')
 }
+
+// warm / pre-cached
+module.exports = () => {
+  require('./list')
+
+  return package
+}
+
+// lazy
+Object.assign(module.exports, package)
